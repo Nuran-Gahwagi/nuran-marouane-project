8cc1d3bd9ff02ca2e6c7470f6c884a2a46c0e53d
Nuran-Gahwagi <nuranzg@gmail.com>
2020-05-14 15:07:44 +0200

    first stage

---
+++ main.js
@@ -0,0 +1,45 @@
+window.visitor;
+$(document).ready(function() { 
+
+var quotes = [
+{   "quote" : "You cannot change the circumstances but you can change yourself.",
+	"writer" : "Jim Rohn"
+},
+
+ { "quote" : "Be yourself, everyone else is already taken.",
+    "writer" : "Oscar Wilde"
+ },
+
+ { "quote" : "Live as if you were to die tomorrow. Learn as if you were to live forever.",
+  "writer" : "Mahatma Gandhi"
+},
+
+];
+
+
+function randomQuotes(arr) {
+return arr[Math.floor(Math.random() * arr.length)];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
